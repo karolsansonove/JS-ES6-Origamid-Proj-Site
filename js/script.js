fe5f3b6e014c181eebb6d9886a08899bed1a2591
Karoline Ribeiro <karolsansonove@gmail.com>
2023-01-19 22:27:46 -0300

    adicionando funções para abrir e fechar modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimationSections from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimationSections();
 initAccordion();
 initTabNav();
+initModal();
