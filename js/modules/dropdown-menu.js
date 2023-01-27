2d5e10b25c081308854821cd48cfac0f1c6a12a5
Karoline Ribeiro <karolsansonove@gmail.com>
2023-01-27 14:03:30 -0300

    adicinando dropdown-menu e clickoutside

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,16 +1,43 @@
-export default function initDropdownMenu() {
+import outsideClick from './outsideclick.js'
 
+export default function initDropdownMenu() {
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+    const active = 'active';
+    const events = ['click', 'touchstart'];
+    
+    
+    dropdownMenus.forEach(menu => {
+        events.forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        });
+    });
+    
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add(active);
+        outsideClick(this, events, () => { // executa a função outsideClick passando menu (this) e callback por parâmetro
+            this.classList.remove(active); // quanto clicar fora do menu, remove classe active
+        });
+    }
 }
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-    ['click', 'touchstart'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick);
-    })
-});
+/*
+// Minha versão do código 
 
 function handleClick(event) {
     event.preventDefault();
-    this.classList.add();
+    this.classList.add(active);
+    outsideClick(this);
 }
+
+function outsideClick(element) {
+    const html = document.documentElement;
+    html.addEventListener('click', handleOutsideClick);
+
+    function handleOutsideClick(event) {
+        if (!element.contains(event.target)) {
+            element.classList.remove(active);
+        }
+    }
+}
+*/
---
+++ js/modules/outsideclick.js
@@ -0,0 +1,21 @@
+export default function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside';
+    
+    if (!element.hasAttribute(outside)) { // se o data-outside existir, significa que o evento já existe, não irá adicionar mais de uma vez
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOutsideClick); // adiciona evento toda vez que ocorre clique ou touchstart no menu (handleClick)
+        });
+        
+        element.setAttribute(outside, '');
+    }
+    function handleOutsideClick(event) {
+        if (!element.contains(event.target)) { // condição: se menu não conter elemento que foi clicado
+            element.removeAttribute(outside); // remove data-outside
+            events.forEach(userEvent => {
+                html.removeEventListener(userEvent, handleOutsideClick); // remove envento junto com remoção da classe active no menu
+            });
+            callback(); // executa callback que remove classe active do menu
+        }
+    }
+}
